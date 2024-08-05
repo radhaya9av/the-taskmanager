3495c651fbf624ed524522d768e7fd1ad062120d
radhaya9av <radhaya9av@gmail.com>
2024-08-05 15:36:44 +0530

    createContextFunctioning

--- app/Components/Modals/CreateContent.tsx
+++ app/Components/Modals/CreateContent.tsx
@@ -1,32 +1,91 @@
 "use client";
 import { useGlobalState } from "@/app/context/globalProvider";
+import axios from "axios";
 import React, { useState } from "react";
+import toast from "react-hot-toast";
 import styled from "styled-components";
 import Button from "../Button/Button";
 import { add, plus } from "@/app/utils/Icons";
 
 function CreateContent() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+  const [completed, setCompleted] = useState(false);
+  const [important, setImportant] = useState(false);
+
   const { theme, allTasks, closeModal } = useGlobalState();
 
+  const handleChange = (name: string) => (e: any) => {
+    switch (name) {
+      case "title":
+        setTitle(e.target.value);
+        break;
+      case "description":
+        setDescription(e.target.value);
+        break;
+      case "date":
+        setDate(e.target.value);
+        break;
+      case "completed":
+        setCompleted(e.target.checked);
+        break;
+      case "important":
+        setImportant(e.target.checked);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const task = {
+      title,
+      description,
+      date,
+      completed,
+      important,
+    };
+
+    try {
+      const res = await axios.post("/api/tasks", task);
+
+      if (res.data.error) {
+        toast.error(res.data.error);
+      }
+
+      if (!res.data.error) {
+        toast.success("Task created successfully.");
+        allTasks();
+        closeModal();
+      }
+    } catch (error) {
+      toast.error("Something went wrong.");
+      console.log(error);
+    }
+  };
+
   return (
-    <CreateContentStyled onSubmit={} theme={theme}>
+    <CreateContentStyled onSubmit={handleSubmit} theme={theme}>
       <h1>Create a Task</h1>
       <div className="input-control">
         <label htmlFor="title">Title</label>
         <input
           type="text"
           id="title"
-          value={}
+          value={title}
           name="title"
-          onChange={("title")}
+          onChange={handleChange("title")}
           placeholder="e.g, Watch a video from Fireship."
         />
       </div>
       <div className="input-control">
         <label htmlFor="description">Description</label>
         <textarea
-          value={}
-          onChange={("description")}
+          value={description}
+          onChange={handleChange("description")}
           name="description"
           id="description"
           rows={4}
@@ -36,8 +95,8 @@ function CreateContent() {
       <div className="input-control">
         <label htmlFor="date">Date</label>
         <input
-          value={}
-          onChange={("date")}
+          value={date}
+          onChange={handleChange("date")}
           type="date"
           name="date"
           id="date"
@@ -47,7 +106,7 @@ function CreateContent() {
         <label htmlFor="completed">Toggle Completed</label>
         <input
           value={completed.toString()}
-          onChange={("completed")}
+          onChange={handleChange("completed")}
           type="checkbox"
           name="completed"
           id="completed"
@@ -57,7 +116,7 @@ function CreateContent() {
         <label htmlFor="important">Toggle Important</label>
         <input
           value={important.toString()}
-          onChange={("important")}
+          onChange={handleChange("important")}
           type="checkbox"
           name="important"
           id="important"
