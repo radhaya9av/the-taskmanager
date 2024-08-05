9de3e692475fdaa942ae9209d578c8a9c51e465f
radhaya9av <radhaya9av@gmail.com>
2024-08-05 15:30:04 +0530

    taskitemComponent

---
+++ app/Components/TaskItem/TaskItem.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { useGlobalState } from "@/app/context/globalProvider";
+import { edit, trash } from "@/app/utils/Icons";
+import React from "react";
+import styled from "styled-components";
+
+interface Props {
+  title: string;
+  description: string;
+  date: string;
+  isCompleted: boolean;
+  id: string;
+}
+
+function TaskItem({ title, description, date, isCompleted, id }: Props) {
+  const { theme, deleteTask, updateTask } = useGlobalState();
+  return (
+    <TaskItemStyled theme={theme}>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <p className="date">{formatDate(date)}</p>
+      <div className="task-footer">
+        {isCompleted ? (
+          <button
+            className="completed"
+            onClick={() => {
+              const task = {
+                id,
+                isCompleted: !isCompleted,
+              };
+
+              updateTask(task);
+            }}
+          >
+            Completed
+          </button>
+        ) : (
+          <button
+            className="incomplete"
+            onClick={() => {
+              const task = {
+                id,
+                isCompleted: !isCompleted,
+              };
+
+              updateTask(task);
+            }}
+          >
+            Incomplete
+          </button>
+        )}
+        <button className="edit">{edit}</button>
+        <button
+          className="delete"
+          onClick={() => {
+            deleteTask(id);
+          }}
+        >
+          {trash}
+        </button>
+      </div>
+    </TaskItemStyled>
+  );
+}
+
+const TaskItemStyled = styled.div;
+
+export default TaskItem;
