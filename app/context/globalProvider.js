855aac7931096ddc01e269cc5cd1bce10b72b9d7
radhaya9av <radhaya9av@gmail.com>
2024-08-05 14:45:59 +0530

    globalProviderContext

---
+++ app/context/globalProvider.js
@@ -0,0 +1,56 @@
+"use client";
+import React, { createContext, useState, useContext } from "react";
+import themes from "./themes";
+import { useUser } from "@clerk/nextjs";
+
+export const GlobalContext = createContext();
+export const GlobalUpdateContext = createContext();
+
+export const GlobalProvider = ({ children }) => {
+  const { user } = useUser();
+
+  const [selectedTheme, setSelectedTheme] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
+
+  const [tasks, setTasks] = useState([]);
+
+  const theme = themes[selectedTheme];
+
+  const completedTasks = tasks.filter((task) => task.isCompleted === true);
+  const importantTasks = tasks.filter((task) => task.isImportant === true);
+  const incompleteTasks = tasks.filter((task) => task.isCompleted === false);
+
+  React.useEffect(() => {
+    if (user) allTasks();
+  }, [user]);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        theme,
+        tasks,
+        deleteTask,
+        isLoading,
+        completedTasks,
+        importantTasks,
+        incompleteTasks,
+        updateTask,
+        modal,
+        openModal,
+        closeModal,
+        allTasks,
+        collapsed,
+        collapseMenu,
+      }}
+    >
+      <GlobalUpdateContext.Provider value={{}}>
+        {children}
+      </GlobalUpdateContext.Provider>
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalState = () => useContext(GlobalContext);
+export const useGlobalUpdate = () => useContext(GlobalUpdateContext);
