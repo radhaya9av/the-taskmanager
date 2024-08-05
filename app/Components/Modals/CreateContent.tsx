93fd036e76cf03ea8f2aaddaf57ef093f4cd1384
radhaya9av <radhaya9av@gmail.com>
2024-08-05 15:37:09 +0530

    createContextStyling

--- app/Components/Modals/CreateContent.tsx
+++ app/Components/Modals/CreateContent.tsx
@@ -139,6 +139,83 @@ function CreateContent() {
   );
 }
 
-const CreateContentStyled = styled.form;
+const CreateContentStyled = styled.form`
+  > h1 {
+    font-size: clamp(1.2rem, 5vw, 1.6rem);
+    font-weight: 600;
+  }
+
+  color: ${(props) => props.theme.colorGrey1};
+
+  .input-control {
+    position: relative;
+    margin: 1.6rem 0;
+    font-weight: 500;
+
+    @media screen and (max-width: 450px) {
+      margin: 1rem 0;
+    }
+
+    label {
+      margin-bottom: 0.5rem;
+      display: inline-block;
+      font-size: clamp(0.9rem, 5vw, 1.2rem);
+
+      span {
+        color: ${(props) => props.theme.colorGrey3};
+      }
+    }
+
+    input,
+    textarea {
+      width: 100%;
+      padding: 1rem;
+
+      resize: none;
+      background-color: ${(props) => props.theme.colorGreyDark};
+      color: ${(props) => props.theme.colorGrey2};
+      border-radius: 0.5rem;
+    }
+  }
+
+  .submit-btn button {
+    transition: all 0.35s ease-in-out;
+
+    @media screen and (max-width: 500px) {
+      font-size: 0.9rem !important;
+      padding: 0.6rem 1rem !important;
+
+      i {
+        font-size: 1.2rem !important;
+        margin-right: 0.5rem !important;
+      }
+    }
+
+    i {
+      color: ${(props) => props.theme.colorGrey0};
+    }
+
+    &:hover {
+      background: ${(props) => props.theme.colorPrimaryGreen} !important;
+      color: ${(props) => props.theme.colorWhite} !important;
+    }
+  }
+
+  .toggler {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    cursor: pointer;
+
+    label {
+      flex: 1;
+    }
+
+    input {
+      width: initial;
+    }
+  }
+`;
 
 export default CreateContent;
